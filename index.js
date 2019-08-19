c5f538699196094e66d54b07b9140aa922f893a0
Bryson Creighton <brysoncreighton@gmail.com>
2019-08-18 21:28:41 -0600

    added funtionality to add new items to the DOM via form entry

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // create a jQuery function
 $(function() {
-    $('js-shopping-list-form').submit(event => {
+    $('#js-shopping-list-form').submit(event => {
 
         // prevent default form functionality
         event.preventDefault();
 
         // create a variable to house the user input data
-        const newItem = $('shopping-list-entry').val();
+        const newItem = $('#shopping-list-entry').val();
         // create variable to house default shopping list item controls
-        const shoppingControls = '<div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div>';
+        const defaultShoppingControls = '<div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="button-label">check</span></button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button></div>';
+
+        // add new item to the DOM
+        $('.shopping-list').append(
+            $(`<li><span class="shopping-item">${newItem}</span>${defaultShoppingControls}</li>`)
+        );
     });
+
+    // toggle the '.shopping-item__checked' class when the 'check" button is clicked
+
+    // remove shopping item when the 'delete' button is clicked
 });
